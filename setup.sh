41a5b7589b152f606aeb51240dd74051b853422c
Chris Cundy <chris.j.cundy@gmail.com>
2017-08-29 12:13:05 -0700

    Added sudo for necessary commands

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #bin/bash
+#This script fetches the necessary 
 
-sudo apt-get install emacs
+sudo apt-get --assume-yes install emacs
 git clone https://github.com/C-J-Cundy/CudnnLSTM-test.git
 git clone https://github.com/C-J-Cundy/.dotfiles.git
-mv ./dotfiles/.emacs.d ~
-mv ./dotfiles/.emacs ~
-mv ./dotfiles/bash_profile ~
-git clone CudnnLSTM-test.git
+sudo mv ./.dotfiles/.emacs.d ~
+sudo mv ./.dotfiles/.emacs ~
+sudo mv ./.dotfiles/bash_profile ~
+https://github.com/eamartin/plr.git
+cd plr/linear_recurrent_net
+sudo cp /usr/local/cuda/lib64/libcudart* /usr/lib/
+./build.sh
+export PYTHONPATH=~/plr/linear_recurrent_net:$PYTHONPATH
+
