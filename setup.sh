788baac8f2406a53b47a4549f2270fd39cbcf3de
Chris Cundy <chris.j.cundy@gmail.com>
2017-08-29 20:26:55 -0700

    bugfixes

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ git clone https://github.com/C-J-Cundy/CudnnLSTM-test.git
 git clone https://github.com/C-J-Cundy/.dotfiles.git
 sudo mv ./.dotfiles/.emacs.d ~
 sudo mv ./.dotfiles/.emacs ~
-sudo mv ./.dotfiles/bash_profile ~
-https://github.com/eamartin/plr.git
+sudo mv ./.dotfiles/.bash_profile ~
+git clone https://github.com/eamartin/plr.git
 cd plr/linear_recurrent_net
 sudo cp /usr/local/cuda/lib64/libcudart* /usr/lib/
 ./build.sh
