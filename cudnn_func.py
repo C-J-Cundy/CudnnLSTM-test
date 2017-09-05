1c2d62bd8d4a4c8fd18c7bcdf0577abebdc3b0b8
Chris Cundy <chris.j.cundy@gmail.com>
2017-09-05 10:57:08 -0700

    Wrapped cudnn into a function

--- cudnn_func.py
+++ cudnn_func.py
@@ -49,162 +49,174 @@ def gen_2b_data(p, q, bs):
     perm = np.random.permutation(bs) #Shuffle order of outputs
     return x_out[:, perm, :], y_out[perm]
 
-def cudnn(n_steps, n_hidden, batch_size, n_layers):
-#Network Parameters
-n_steps = 1024
-n_hidden = 1024
-n_input = 1025
-n_classes = 2
-n_layers = 1
-#sn = math.sqrt(1.0)/math.sqrt(n_input+n_hidden) #Glorot initialisation, var(p(x))
-sn = 0.05 #Glorot initialisation, var(p(x))
-forget_gate_init = 5.0                          # = 1/(n_in). We use uniform p(x)
-clip = 20 #We use gradient clipping to stop the gradient exploding initially
-         #for the much larger networks
+def cudnn(n_steps=1024, n_hidden=1024, batch_size=8, n_layers=1):
+    #Network Parameters
+    n_classes = 2
+    sn = 0.05 #Glorot initialisation, var(p(x))
+    forget_gate_init = 5.0                          # = 1/(n_in). We use uniform p(x)
+    clip = 20 #We use gradient clipping to stop the gradient exploding initially
+             #for the much larger networks
 
 
-#Training Parameters
-learning_rate = 0.0001
-training_iters = 5000000
-batch_size = 8
-display_step = 10
+    #Training Parameters
+    learning_rate = 0.0001
+    training_iters = 5000000
+    batch_size = 8
+    display_step = 10
 
 
-#Initialise variables
-################################################################################
-#Generate the lstm hook to CUDA
-model = tf.contrib.cudnn_rnn.CudnnLSTM(n_layers, n_hidden, n_input)
+    #Initialise variables
+    ################################################################################
+    #Generate the lstm hook to CUDA
+    model = tf.contrib.cudnn_rnn.CudnnLSTM(n_layers, n_hidden, n_input)
 
-# tf Graph input
-x = tf.placeholder("float", [n_steps, batch_size, n_input])
-y = tf.placeholder("float", [batch_size, n_classes])
+    # tf Graph input
+    x = tf.placeholder("float", [n_steps, batch_size, n_input])
+    y = tf.placeholder("float", [batch_size, n_classes])
 
-#Define weights & rnn initial states
-weights = {
-    'out': tf.Variable(tf.random_normal([n_hidden, n_classes]), dtype='float')
-}
-biases = {
-    'out': tf.Variable(tf.random_normal([n_classes]), dtype='float')
-}
-#Initial state of the LSTM at each batch, we don't let this be trained. 
-input_h = {
-    'out': tf.Variable(tf.zeros([n_layers, batch_size, n_hidden]), dtype='float',
-                       trainable=False)
-}
-input_c = {
-    'out': tf.Variable(tf.zeros([n_layers, batch_size, n_hidden]), dtype='float',
-                       trainable=False)
-}
-#Initialise all weights & biases for the cudnnlstm: set weights according to Glorot
-#There are eight weights and biases per layer in the LSTM. Described in 
-#http://docs.nvidia.com/deeplearning/sdk/cudnn-user-guide/index.html#cudnnRNNMode_t
-#There are two biases which sum to give the biases in the canonical form of the LSTM
-#This seems redundant - I'm not sure why CUDA is implemented in this way.
+    #Define weights & rnn initial states
+    weights = {
+        'out': tf.Variable(tf.random_normal([n_hidden, n_classes]), dtype='float')
+    }
+    biases = {
+        'out': tf.Variable(tf.random_normal([n_classes]), dtype='float')
+    }
+    #Initial state of the LSTM at each batch, we don't let this be trained. 
+    input_h = {
+        'out': tf.Variable(tf.zeros([n_layers, batch_size, n_hidden]), dtype='float',
+                           trainable=False)
+    }
+    input_c = {
+        'out': tf.Variable(tf.zeros([n_layers, batch_size, n_hidden]), dtype='float',
+                           trainable=False)
+    }
+    #Initialise all weights & biases for the cudnnlstm: set weights according to Glorot
+    #There are eight weights and biases per layer in the LSTM. Described in 
+    #http://docs.nvidia.com/deeplearning/sdk/cudnn-user-guide/index.html#cudnnRNNMode_t
+    #There are two biases which sum to give the biases in the canonical form of the LSTM
+    #This seems redundant - I'm not sure why CUDA is implemented in this way.
 
-weight_list = []
-bias_list = []
-for n in range(4):
-    weight_list.append(np.float32(
-        np.random.uniform(low=-sn, high=sn,
-                          size=[n_hidden, n_input])))
+    weight_list = []
+    bias_list = []
+    for n in range(4):
+        weight_list.append(np.float32(
+            np.random.uniform(low=-sn, high=sn,
+                              size=[n_hidden, n_input])))
 
-for n in range(4,8):
-    weight_list.append(np.float32(
-        np.random.uniform(low=-sn, high=sn,
-                          size=[n_hidden, n_hidden])))
+    for n in range(4,8):
+        weight_list.append(np.float32(
+            np.random.uniform(low=-sn, high=sn,
+                              size=[n_hidden, n_hidden])))
 
-for n in range(8):
-    bias_list.append(np.float32(
-        np.zeros([n_hidden])))
+    for n in range(8):
+        bias_list.append(np.float32(
+            np.zeros([n_hidden])))
 
-bias_list[5] = np.float32(
-        forget_gate_init*np.ones([n_hidden]))
+    bias_list[5] = np.float32(
+            forget_gate_init*np.ones([n_hidden]))
 
-#Initialize the opaque parameter buffer used to handle the cudnnlstm params
-#If we try to pass the canonical_to_params tensor through the call graph,
-#we fail because the size must be known statically. The easiest way to get
-#around this (though hacky) is to get the values out by casting to an np array
-#and then initialising a tensor with those values.
+    #Initialize the opaque parameter buffer used to handle the cudnnlstm params
+    #If we try to pass the canonical_to_params tensor through the call graph,
+    #we fail because the size must be known statically. The easiest way to get
+    #around this (though hacky) is to get the values out by casting to an np array
+    #and then initialising a tensor with those values.
 
-params_size_t = (  (n_input * n_hidden * 4) 
-                 + (n_hidden * n_hidden * 4)
-                 + (n_hidden * 2 * 4))
-flat_params = model.canonical_to_params(weight_list, bias_list)
-flat_params_as_ndarray = tf.Session().run(flat_params) 
+    params_size_t = (  (n_input * n_hidden * 4) 
+                     + (n_hidden * n_hidden * 4)
+                     + (n_hidden * 2 * 4))
+    flat_params = model.canonical_to_params(weight_list, bias_list)
+    flat_params_as_ndarray = tf.Session().run(flat_params) 
 
-params = {
-    'out': tf.get_variable('param_buffer', initializer=tf.constant(
-        flat_params_as_ndarray))
-}
-
-if n_layers == 2:
-    params2 = {
-        'out': tf.get_variable('param_buffer_2', initializer=tf.constant(
+    params = {
+        'out': tf.get_variable('param_buffer', initializer=tf.constant(
             flat_params_as_ndarray))
     }
-    model2 = tf.contrib.cudnn_rnn.CudnnLSTM(n_layers, n_hidden, n_input)
 
-#Generate network
-################################################################################
+    if n_layers == 2:
+        #We need to change the dimensionality of the input here
+        model2 = tf.contrib.cudnn_rnn.CudnnLSTM(n_layers, n_hidden, n_input)    
+        weight_list = []
+        for n in range(0,8):
+            weight_list.append(np.float32(
+                np.random.uniform(low=-sn, high=sn,
+                                  size=[n_hidden, n_hidden])))
+        flat_params_as_ndarray = tf.Session().run(model.canonical_to_params(
+            weight_list, bias_list))         
+        params2 = {
+            'out': tf.get_variable('param_buffer_2', initializer=tf.constant(
+                flat_params_as_ndarray))
+        }
 
-outputs, states1, states2 = model(
-    is_training=True,
-    input_data=x,
-    input_h=input_h['out'],
-    input_c=input_c['out'],
-    params=params['out'])
 
-if n_layers == 2:
-    outputs, states1, states2 = model2(
+    #Generate network
+    ################################################################################
+
+    outputs, states1, states2 = model(
         is_training=True,
-        input_data=outputs,
+        input_data=x,
         input_h=input_h['out'],
         input_c=input_c['out'],
-        params=params2['out'])
+        params=params['out'])
 
-# Linear activation, using rnn inner loop on last output
-pred = tf.matmul(outputs[-1], weights['out']) + biases['out']    
+    if n_layers == 2:
+        outputs, states1, states2 = model2(
+            is_training=True,
+            input_data=outputs,
+            input_h=input_h['out'],
+            input_c=input_c['out'],
+            params=params2['out'])
 
-#Evaluate network, run adam and clip gradients
-################################################################################
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-optimizer_0 = tf.train.AdamOptimizer(learning_rate=learning_rate)
-raw_gradients, variables = zip(*optimizer_0.compute_gradients(cost))
-gradients, _ = tf.clip_by_global_norm(raw_gradients, clip)
-optimizer = optimizer_0.apply_gradients(zip(gradients, variables))
-correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-tf.summary.scalar('cost', cost)
-tf.summary.scalar('acc', accuracy)
-init = tf.global_variables_initializer()
-merged = tf.summary.merge_all()
-init = tf.global_variables_initializer()
-saver = tf.train.Saver()
+    # Linear activation, using rnn inner loop on last output
+    pred = tf.matmul(outputs[-1], weights['out']) + biases['out']    
 
+    #Evaluate network, run adam and clip gradients
+    ################################################################################
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    optimizer_0 = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    raw_gradients, variables = zip(*optimizer_0.compute_gradients(cost))
+    gradients, _ = tf.clip_by_global_norm(raw_gradients, clip)
+    optimizer = optimizer_0.apply_gradients(zip(gradients, variables))
+    correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    tf.summary.scalar('cost', cost)
+    tf.summary.scalar('acc', accuracy)
+    init = tf.global_variables_initializer()
+    merged = tf.summary.merge_all()
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
 
-with tf.device("gpu:0"):
-    with tf.Session() as sess:
-        sess.run(init)
-        step = 1
-        test_writer = tf.summary.FileWriter('./CudnnLSTM_'+str(n_steps)+'_stepslog', sess.graph)
-        # Keep training until reach max iterations
-        while step * batch_size < training_iters:
-            batch_x, batch_y = gen_2b_data(n_steps-1, n_input-1, batch_size)
-            # Run optimization op (backprop)
-            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-            if step % display_step == 0:
-                # Calculate batch accuracy
-                acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
-                # Calculate batch loss
-                loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
-                summary, _ = sess.run([merged, cost], feed_dict={x: batch_x, y: batch_y})
-                summary, _ = sess.run([merged, accuracy], feed_dict={x: batch_x, y: batch_y})
-                test_writer.add_summary(summary, step)
-                print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
-                      "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                      "{:.5f}".format(acc))
-                if step % (display_step*10) == 0: #Save the model every so often
-                    saver.save(sess, './CudnnLSTM_'+str(n_steps)+'_steps_model', global_step=step)
-            step += 1
-                    
-        print("Optimization Finished!")
+    start = time.time()
+    acc_list = [0]*5
+    with tf.device("gpu:0"):
+        with tf.Session() as sess:
+            sess.run(init)
+            step = 1
+            test_writer = tf.summary.FileWriter('./CudnnLSTM_'+str(n_steps)+'_stepslog', sess.graph)
+            # Keep training until reach max iterations
+            while step * batch_size < training_iters:
+                batch_x, batch_y = gen_2b_data(n_steps-1, n_input-1, batch_size)
+                # Run optimization op (backprop)
+                sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+                if step % display_step == 0:
+                    # Calculate batch accuracy
+                    acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+                    # Calculate batch loss
+                    loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+                    summary, _ = sess.run([merged, cost], feed_dict={x: batch_x, y: batch_y})
+                    summary, _ = sess.run([merged, accuracy], feed_dict={x: batch_x, y: batch_y})
+                    test_writer.add_summary(summary, step)
+                    print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                          "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                          "{:.5f}".format(acc))
+                    if step % (display_step*10) == 0: #Save the model every so often
+                        saver.save(sess, './CudnnLSTM_'+str(n_steps)+'_steps_model', global_step=step)
+                    if acc_list == [1.0]*5:
+                        print "Converged after {} iterations and {} seconds".format(step*batch_size, time.time() - start)
+                    break
+                else:
+                    acc_list.append(acc)
+                    acc_list.pop(0)     
+                step += 1
+
+            print("Optimization Finished!")
+    return step*batch_size, time.time()-start
