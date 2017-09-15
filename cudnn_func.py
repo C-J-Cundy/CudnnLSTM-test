247ee3dcf50260fb708460e5b14b11e5334770d0
Chris Cundy <chris.j.cundy@gmail.com>
2017-09-15 15:25:09 -0700

    Added bs=1 case

--- cudnn_func.py
+++ cudnn_func.py
@@ -49,11 +49,29 @@ def gen_2b_data(p, q, bs):
     perm = np.random.permutation(bs) #Shuffle order of outputs
     return x_out[:, perm, :], y_out[perm]
 
+def gen_2b_data_1(p, q):
+    """Generates data with a batch size of 1"""
+    x_out = np.zeros((p+1, 1, q+1))
+    y_out = np.zeros((1,2))
+    seq_class = np.random.random_integers(2) - 1
+    if seq_class == 0:
+        x_out[:,0,:] = np.eye(q+1)[np.random.choice(q+1, p+1)] #Random one-hot
+        x_out[0,0,:] = np.zeros(q+1) 
+        x_out[0,0,0] = 1 #Set indicator component
+        y_out[0, 0] = 1 
+    else:
+        x_out[:,0,:] = np.eye(q+1)[np.random.choice(q+1, p+1)]
+        x_out[0,0,:] = np.zeros(q+1)
+        x_out[0,0,0] = -1
+        y_out[0, 1] = 1
+    return x_out, y_out
+
+
 def cudnn(n_steps=1024, n_hidden=1024, n_input=128, batch_size=8, n_layers=1):
     #Network Parameters
     tf.reset_default_graph()
     n_classes = 2
-    sn = 1/math.sqrt(n_hidden) #Glorot initialisation, var(p(x))
+    sn = 0.1 #Glorot initialisation
     forget_gate_init = 5.0                          # = 1/(n_in). We use uniform p(x)
     clip = 20 #We use gradient clipping to stop the gradient exploding initially
              #for the much larger networks
@@ -151,14 +169,12 @@ def cudnn(n_steps=1024, n_hidden=1024, n_input=128, batch_size=8, n_layers=1):
 
     #Generate network
     ################################################################################
-
     outputs, states1, states2 = model(
         is_training=True,
         input_data=x,
         input_h=input_h['out'],
         input_c=input_c['out'],
         params=params['out'])
-
 
     # Linear activation, using rnn inner loop on last output
     pred = tf.matmul(outputs[-1], weights['out']) + biases['out']    
@@ -180,7 +196,11 @@ def cudnn(n_steps=1024, n_hidden=1024, n_input=128, batch_size=8, n_layers=1):
     saver = tf.train.Saver()
 
     start = time.time()
-    acc_list = [0]*5
+    if batch_size == 1:
+        conv_criterion = 15
+    else:
+        conv_criterion = 5    
+    acc_list = [0]*conv_criterion
     with tf.device("gpu:0"):
         with tf.Session() as sess:
             sess.run(init)
@@ -188,7 +208,10 @@ def cudnn(n_steps=1024, n_hidden=1024, n_input=128, batch_size=8, n_layers=1):
             test_writer = tf.summary.FileWriter('./CudnnLSTM_'+str(n_steps)+'_stepslog', sess.graph)
             # Keep training until reach max iterations
             while step * batch_size < training_iters:
-                batch_x, batch_y = gen_2b_data(n_steps-1, n_input-1, batch_size)
+                if batch_size == 1:
+                    batch_x, batch_y = gen_2b_data_1(n_steps-1, n_input-1)
+                else:
+                    batch_x, batch_y = gen_2b_data(n_steps-1, n_input-1, batch_size)
                 # Run optimization op (backprop)
                 sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
                 if step % display_step == 0:
@@ -204,7 +227,7 @@ def cudnn(n_steps=1024, n_hidden=1024, n_input=128, batch_size=8, n_layers=1):
                           "{:.5f}".format(acc))
                     if step % (display_step*10) == 0: #Save the model every so often
                         saver.save(sess, './CudnnLSTM_'+str(n_steps)+'_steps_model', global_step=step)
-                    if acc_list == [1.0]*5:
+                    if acc_list == [1.0]*conv_criterion:
                         print "Converged after {} iterations and {} seconds".format(step*batch_size, time.time() - start)
                         break
                     else:
