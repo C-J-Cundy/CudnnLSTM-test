4ba410365544e457aaaab0c3fb5b9930f2f28ec8
Chris Cundy <chris.j.cundy@gmail.com>
2017-09-23 09:37:38 -0700

    Added clipping

--- test_master.py
+++ test_master.py
@@ -8,8 +8,11 @@ bs_cudn_dict = {1024: 8, 8192:4, 1048576:1} #Batch sizes that work best: found b
 bs_lslstm_dict = {1024: 8, 8192:4, 1048576:1} #Batch sizes that work best: found by quick search.
 n_hidden_dict = {1024: 512, 8192: 512, 1048576: 64} #Need to reduce the num of hidden layers
                                                     #So that the model fits in memory
-n_converge_dict = {1024:10, 8192: 30, 1048576: 50} #Number of 100% minibatches to say we've converged 
-sn_dict = {1024: 0.1, 8192: 0.05, 1048576: 64} #Need to reduce the num of hidden layers                                                    
+n_converge_dict = {1024:20, 8192: 50, 1048576: 75} #Number of 100% minibatches to say we've converged
+                                                   #We might use longer than necessary for good-looking                                                   #Graphs 
+sn_dict = {1024: 0.1, 8192: 0.05, 1048576: 64} #Need to reduce the num of hidden layers
+clip_dict = {1024: 100, 8192: 10, 1048576: 2} #Need to clip for the cudnn for longer seqlen
+
 for n_step in n_steps:
     #Do the iteration for the  2-layer cudnn
     #Make sure that the sn is set to 0.03 for 8192 for the cuda
@@ -17,7 +20,8 @@ for n_step in n_steps:
     iter_list = []
     times_list = []
     for _ in range(num_trials):
-        a, b = cudnn(n_step, n_hidden, 128, bs_cudn_dict[n_step], 2, n_converge_dict[n_step])
+        a, b = cudnn(n_step, n_hidden, 128, bs_cudn_dict[n_step],
+                     2, n_converge_dict[n_step], clip_dict[n_step])
         iter_list.append(a)
         times_list.append(b)
         print "Took {} seconds to converge after {} iterations".format(b, a)
