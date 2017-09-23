a077639bbdb7721214be8a70a2aee27f03731aac
Chris Cundy <chris.j.cundy@gmail.com>
2017-09-22 17:22:26 -0700

    bugfix for convergence

--- cudnn_func.py
+++ cudnn_func.py
@@ -197,10 +197,6 @@ def cudnn(n_steps=1024, n_hidden=1024, n_input=128, batch_size=8, n_layers=1, n_
     saver = tf.train.Saver()
 
     start = time.time()
-    if batch_size == 1:
-        n_converge = 15
-    else:
-        n_converge = 5    
     acc_list = [0]*n_converge
     with tf.device("gpu:0"):
         with tf.Session() as sess:
