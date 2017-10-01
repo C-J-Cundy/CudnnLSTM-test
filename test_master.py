bd97dd0a018f1932f560bf6cf16c474be227d7d7
Chris Cundy <chris.j.cundy@gmail.com>
2017-10-01 14:15:34 -0700

    Modified forget gate in order for 8k to converge

--- test_master.py
+++ test_master.py
@@ -10,8 +10,9 @@ n_hidden_dict = {1024: 512, 8192: 512, 1048576: 64} #Need to reduce the num of h
                                                     #So that the model fits in memory
 n_converge_dict = {1024:20, 8192: 50, 1048576: 75} #Number of 100% minibatches to say we've converged
                                                    #We might use longer than necessary for good-looking                                                   #Graphs 
-sn_dict = {1024: 0.1, 8192: 0.05, 1048576: 64} #Need to reduce the num of hidden layers
+sn_dict = {1024: 0.1, 8192: 0.08, 1048576: 0.1} #Need to reduce the num of hidden layers
 clip_dict = {1024: 100, 8192: 10, 1048576: 2} #Need to clip for the cudnn for longer seqlen
+fg_dict = {1024: 5.0, 8192: 10.0, 1048576: 2} #Need to clip for the cudnn for longer seqlen
 
 for n_step in n_steps:
     #Do the iteration for the  2-layer cudnn
@@ -21,7 +22,8 @@ for n_step in n_steps:
     times_list = []
     for _ in range(num_trials):
         a, b = cudnn(n_step, n_hidden, 128, bs_cudn_dict[n_step],
-                     2, n_converge_dict[n_step], clip_dict[n_step], sn_dict[n_step])
+                     2, n_converge_dict[n_step], clip_dict[n_step], sn_dict[n_step],
+                     fg_dict[n_step])
         iter_list.append(a)
         times_list.append(b)
         print "Took {} seconds to converge after {} iterations".format(b, a)
@@ -58,6 +60,3 @@ for n_step in n_steps:
     # to converge""".format(num_trials, n_step, n_hidden, np.mean(iter_list),           #
     #                       np.std(iter_list), np.mean(times_list), np.std(times_list)) #
     #####################################################################################
-
-
-    
